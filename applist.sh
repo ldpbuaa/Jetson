a5927e64051c2578cb299290d0f98afc0d75051b
ldpbuaa <ldpbuaa@163.com>
2018-04-26 01:42:16 +0000

    update applist.sh & add sources.list.bak

---
+++ applist.sh
@@ -0,0 +1,6 @@
+sudo apt install -y mate-system-monitor
+sudo apt install -y indicator-multiload
+sudo apt install -y indicator-cpufreq
+sudo apt install -y unity-tweak-tool
+sudo apt install -y fcitx
+sudo apt install -y aptitude
