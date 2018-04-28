e226255603b08a01125cf24bcf242ff74fc2f3bc
ldpbuaa <ldpbuaa@163.com>
2018-04-28 15:27:45 +0000

    update applish.sh

--- applist.sh
+++ applist.sh
@@ -3,5 +3,6 @@ sudo apt install -y indicator-multiload
 sudo apt install -y indicator-cpufreq
 sudo apt install -y unity-tweak-tool
 sudo apt install -y fcitx
+sudo apt install -y fcitx-googlepinyin
 sudo apt install -y aptitude
 sudo apt install -y libavahi-compat-libdnssd1 # install necessary lib for synergy
